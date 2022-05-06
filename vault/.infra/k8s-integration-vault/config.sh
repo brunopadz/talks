56d7a028c6c33f46823cb2b7fd360dd8744e8a41
brunopadz <bpadz@protonmail.com>
2022-05-06 09:57:45 -0300

    Clean references to internal domain

--- vault/.infra/k8s-integration-vault/config.sh
+++ vault/.infra/k8s-integration-vault/config.sh
@@ -1,4 +1,4 @@
-VAULT_ADDR="http://localhost:8200"
+VAULT_ADDR="http://vault.mydomain.com"
 TOKEN_REVIEW_JWT=$(kubectl get secret vault-auth -o go-template='{{ .data.token }}' | base64 --decode)
 KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 --decode)
 KUBE_HOST=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.server}')
